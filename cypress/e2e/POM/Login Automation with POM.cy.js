b71e5edcf25b11edbdeac713651f9e28b3e9a728
unknown <bagasyana61@gmail.com>
2025-10-01 22:35:45 +0700

    reorganize POM folder structure for automation login tests

--- cypress/e2e/POM/Login Automation with POM.cy.js
+++ cypress/e2e/POM/Login Automation with POM.cy.js
@@ -1,143 +1,104 @@
-import pom_code from './POM_page';
+import login_pom from '../../support/POM_commands/login_pom'
+import dataPOM from '../../fixtures/dataPOM.json'
 
 describe('Login Scenario', () => {
   beforeEach(() => {
-    pom_code.visit_login();
+    login_pom.visit_login();
   });
 
   it('TC001 - The user logs in using a valid username and password', () => {
-    pom_code.login('Admin', 'admin123')
-
+    login_pom.login(dataPOM.valid_username, dataPOM.valid_password)
     // ✅ Expected result
-    cy.url().should('include', '/dashboard')
-    cy.contains('Time at Work').should('be.visible')
+    login_pom.login_success()
   })
 
   it('TC002 - The user logs in using an invalid username and password', () => {
-
-    pom_code.login('Adminsalah', 'admin123salah')
-
+    login_pom.login(dataPOM.invalid_username, dataPOM.invalid_password)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.error_message().should('have.text', 'Invalid credentials')
+    login_pom.login_invalid_credentials()
   })
 
   it('TC003 - The user logs in using an invalid username and a valid password', () => {
-
-    pom_code.login('Adminsalah', 'admin123')
-
+    login_pom.login(dataPOM.invalid_username, dataPOM.valid_password)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.error_message().should('have.text', 'Invalid credentials')
+    login_pom.login_invalid_credentials()
   })
 
   it('TC004 - The user logs in using a valid username and an invalid password', () => {
-
-    pom_code.login('Admin', 'admin123salah')
-
+    login_pom.login(dataPOM.valid_username, dataPOM.invalid_password)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.error_message().should('have.text', 'Invalid credentials')
+    login_pom.login_invalid_credentials()
   })
 
   it('TC005 - The user logs in using a valid username and the password in uppercase letters', () => {
-
-    pom_code.login('Admin', 'ADMIN123')
-
+    login_pom.login(dataPOM.valid_username, dataPOM.uppercase_password)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.error_message().should('have.text', 'Invalid credentials')
+    login_pom.login_invalid_credentials()
   })
 
   it('TC006 - The user logs in using an empty username and password', () => {
-
-    pom_code.click_login()
-
+    login_pom.elements.login_button()
+    login_pom.click_login()
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.required_message().should('have.length', 2).and('contain.text', 'Required')
+    login_pom.login_required2()
   })
 
   it('TC007 - The user logs in using a valid username and an empty password', () => {
-
-    pom_code.type_username('Admin')
-    pom_code.click_login()
-
+    login_pom.type_username(dataPOM.valid_username)
+    login_pom.elements.login_button()
+    login_pom.click_login()
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.required_message().should('have.length', 1).and('contain.text', 'Required')
+    login_pom.login_required1()
   })
 
   it('TC008 - The user logs in using an empty username and a valid password', () => {
-
-    pom_code.type_password('admin123')
-    pom_code.click_login()
-
+    login_pom.type_password(dataPOM.valid_password)
+    login_pom.elements.login_button()
+    login_pom.click_login()
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.required_message().should('have.length', 1).and('contain.text', 'Required')
+    login_pom.login_required1()
   })
 
   it('TC009 - The user logs in using both a whitespace username and a password', () => {
-
-    pom_code.login(' ', ' ')
-
+    login_pom.login(dataPOM.whitespace, dataPOM.whitespace)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.required_message().should('have.length', 2).and('contain.text', 'Required')
+    login_pom.login_required2()
   })
   
   it('TC0010 - The user logs in using a whitespace username and a valid password', () => {
-
-    pom_code.login(' ', 'admin123')
-
+    login_pom.login(dataPOM.whitespace, dataPOM.valid_password)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.required_message().should('have.length', 1).and('contain.text', 'Required')
+    login_pom.login_required1()
   })
 
   it('TC011 - The user logs in using a valid username and a whitespasce password', () => {
-
-    pom_code.login('Admin', ' ')
-
+    login_pom.login(dataPOM.valid_username, dataPOM.whitespace)
     // ✅ Expected result
     cy.url().should('include', '/login')
-    pom_code.elements.required_message().should('have.length', 1).and('contain.text', 'Required')
+    login_pom.login_required1()
   })
 
   it('TC012 - The user logs in using a valid username and a valid password with spaces', () => {
-
-    pom_code.login('Admin', 'admin123 ')
-
+    login_pom.login(dataPOM.valid_username, dataPOM.valid_password_with_space)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.error_message().should('have.text', 'Invalid credentials')
+    login_pom.login_invalid_credentials()
   })
 
   it('TC013 - The user logs in using special characters as the username and password', () => {
-
-    pom_code.login('@dmin', '@dmin123')
-
+    login_pom.login(dataPOM.spesial_username, dataPOM.spesial_password)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.error_message().should('have.text', 'Invalid credentials')
+    login_pom.login_invalid_credentials()
   })
 
   it('TC014 - The user logs in using special characters as the username and a valid password', () => {
-
-    pom_code.login('@dmin', 'admin123')
-
+    login_pom.login(dataPOM.spesial_username, dataPOM.valid_password)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.error_message().should('have.text', 'Invalid credentials')
+    login_pom.login_invalid_credentials()
   })
 
   it('TC015 - The user logs in using a valid username and the password using special characters', () => {
-
-    pom_code.login('Admin', '@dmin123')
-
+    login_pom.login(dataPOM.valid_username, dataPOM.spesial_password)
     // ✅ Expected result
-    cy.url().should('include', '/login')
-    pom_code.elements.error_message().should('have.text', 'Invalid credentials')
+    login_pom.login_invalid_credentials()
   })
 })
